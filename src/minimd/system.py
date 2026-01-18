01eee398b3285addb9e4b4c2e01e2dd0dbd41911
Daniel <daniel.ryrz.stud@pw.edu.pl>
2026-01-18 15:34:59 +0000

    Add ParticleSystem class with basic sanity checks and test

--- src/minimd/system.py
+++ src/minimd/system.py
@@ -1,8 +1,28 @@
 """
-Placeholder for ParticleSystem class.
+ParticleSystem class for minimd
+Holds particle positions, velocities, and masses.
 """
+import numpy as np
+
 class ParticleSystem:
-    def __init__(self):
-        self.positions = []
-        self.velocities = []
-        self.masses = []
+    def __init__(self, positions, velocities, masses):
+        """
+        Initialize the particle system.
+
+        Args:
+            positions (np.ndarray): shape (N, 3)
+            velocities (np.ndarray): shape (N, 3)
+            masses (np.ndarray): shape(N,)
+        """
+    
+        self.positions = np.array(positions, dtype=float)
+        self.velocities = np.array(velocities, dtype=float)
+        self.masses = np.array(masses, dtype=float)
+
+        # sanity checks
+        assert self.positions.shape == self.velocities.shape, "Shape of positions and velocities must match"
+        assert self.positions.shape[0] == self.masses.shape[0], "Number of masses instances must match number of particles"
+
+    def num_particles(self):
+        """Return the number of particles"""
+        return self.positions.shape[0]
